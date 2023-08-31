024c9565f5665838543f404eb61d9c66ae8c7e75
Mike Attara <31483629+mikeattara@users.noreply.github.com>
2023-08-31 23:32:25 +0000

    refactor smart contracts (#2)

--- backend/contracts/Donation.sol
+++ backend/contracts/Donation.sol
@@ -13,6 +13,7 @@ contract DonationApp is Ownable {
     struct Donation {
         uint256 causeId;
         uint256 amount;
+        string message;
         address payable donor;
         uint256 date;
     }
@@ -27,6 +28,7 @@ contract DonationApp is Ownable {
     event DonationMade(
         uint256 indexed causeId,
         uint256 amount,
+        string message,
         address indexed donor,
         uint256 date
     );
@@ -40,7 +42,14 @@ contract DonationApp is Ownable {
 
     mapping(uint256 => Donation) public donations;
     mapping(uint256 => Cause) public causes;
+    mapping(address => uint256) donors;
+    uint256 public totalDonations;
     address[] public donorsList;
+
+    modifier amountGreaterThanZero(uint256 _amount) {
+        require(_amount > 0, "Amount must be greater than 0");
+        _;
+    }
 
     function addCause(
         string memory _name,
@@ -63,38 +72,55 @@ contract DonationApp is Ownable {
         causes[_id] = cause;
     }
 
-    function donateToCause(uint256 _causeId) public payable {
+    function donateToCause(
+        uint256 _causeId,
+        uint256 _amount,
+        string calldata _message // optional
+    ) public payable amountGreaterThanZero(_amount) {
         Cause cause = causes[_causeId];
         require(address(cause) != address(0), "Cause does not exist");
         require(cause.paused() == false, "Donations are paused for this cause");
         require(cause.expired() == false, "This cause has already expired");
-        require(msg.value > 0, "Donation amount must be greater than 0");
+        require(_amount > msg.value, "Insufficient funds");
 
         _donationIds.increment();
         uint256 _id = _donationIds.current();
         Donation memory donation = Donation(
             _causeId,
-            msg.value,
+            _amount,
+            _message,
             payable(msg.sender),
             block.timestamp
         );
         donations[_id] = donation;
-        cause.addDonation(msg.value);
-        donorsList.push(msg.sender);
-        emit DonationMade(_causeId, msg.value, msg.sender, block.timestamp);
+        cause.addDonation(_amount);
+        totalDonations += _amount;
+        if (donors[msg.sender] == 0) {
+            donors[msg.sender] = _amount;
+            donorsList.push(msg.sender);
+        } else {
+            donors[msg.sender] += _amount;
+        }
+        emit DonationMade(
+            _causeId,
+            _amount,
+            _message,
+            msg.sender,
+            block.timestamp
+        );
     }
 
     function withdrawFromCause(
         uint256 _causeId,
         uint256 _amount,
-        string calldata _reason
-    ) public payable onlyOwner {
+        string calldata _reason //required
+    ) public payable amountGreaterThanZero(_amount) onlyOwner {
         require(
             causes[_causeId].beneficiary() == msg.sender,
             "Only the beneficiary can withdraw donations"
         );
         require(
-            causes[_causeId].totalDonations() >= _amount,
+            causes[_causeId].balance() >= _amount,
             "Withdrawal amount must be less than or equal to total donations"
         );
         require(
@@ -105,10 +131,6 @@ contract DonationApp is Ownable {
         cause.deductDonation(_amount);
         payable(msg.sender).transfer(_amount);
         emit WithdrawalMade(_causeId, _amount, _reason, block.timestamp);
-    }
-
-    function getDonors() public view returns (address[] memory) {
-        return donorsList;
     }
 
     function getAllCauses() public view returns (Cause[] memory) {
