e7b114c6ffff47bae7e3c2f096a41ec7a6383301
Mike Attara <mpyebattara@gmail.com>
2023-08-31 21:15:59 +0000

    update config

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -3,6 +3,24 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  gasReporter: {
+    currency: "USD",
+    gasPrice: 21,
+    enabled: true,
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [],
+    },
+    matic: {
+      url: "https://rpc-mainnet.maticvigil.com",
+      accounts: [],
+    },
+  },
 };
 
 export default config;
